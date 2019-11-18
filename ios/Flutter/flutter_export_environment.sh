b1ff5387d83955c1d8f8ee01f75eff56e54924ac
afiq3821 <afiq3821@gmail.com>
2019-11-18 10:16:09 +0700

    Initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/gaktau/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gaktau/Documents/newflutter/flutterkartunama/stopwatch-flutter-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gaktau/Documents/flutter/bin/cache/artifacts/engine/ios"
